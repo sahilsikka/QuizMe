01d4ec9badb1bdec86f398d06baf8d4e95a4e3a6
Param Mehta <param.mehta95@gmail.com>
2017-11-22 19:07:16 -0700

    Quiz History Visualization

--- src/app/layout/quiz-history/quiz-history.component.ts
+++ src/app/layout/quiz-history/quiz-history.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {routerTransition} from '../../router.animations';
 import {BackendService} from '../../backend.service';
 import {Router} from '@angular/router';
+import {Color} from 'ng2-charts';
 
 @Component({
     selector: 'app-quiz-history',
@@ -11,26 +12,43 @@ import {Router} from '@angular/router';
 })
 
 export class QuizHistoryComponent implements OnInit {
+    yourTotalScore: number;
     user = JSON.parse(localStorage.getItem('currentUser'));
     userId = this.user.id;
     result: any;
+    totalQuiz: number;
+    totalScore: number;
+    options = {
+        cutoutPercentage: 70,
+        responsive: true,
+        elements: {
+            center: {
+                text: '90%',
+                color: '#FF6384',
+                fontStyle: 'Arial',
+                sidePadding: 20,
+            }
+        }
 
+    };
+    colorsOverride: Array<Color> = [{
+        backgroundColor: ['#2ec866', 'lightgrey'],
+        hoverBackgroundColor: ['#2ec866', 'lightgrey']
+    }];
     constructor(public router: Router, private backend: BackendService) {
     }
 
     ngOnInit() {
+        let i: number;
         this.backend.getQuizHistory(this.userId)
             .subscribe(status => {
+                this.yourTotalScore = 0;
                 this.result = status;
-                console.log(this.result);
+                for (i = 0; i < this.result.length; i++) {
+                    this.yourTotalScore = this.yourTotalScore + this.result[i].score;
+                };
+                this.totalScore = this.result.length * 10 - this.yourTotalScore;
+                this.totalQuiz = this.result.length;
             });
-    }
-
-    quizHistory() {
-        // this.result =  this.backend.getQuizHistory(this.userId);
-        // .subscribe(result => this.result = result.json());
-        // console.log(this.result);
-
-
     }
 }
