2cd7bc260a6d69b5e4cbc63aa1e7286117df716f
sahil <sikka.sahil@gmail.com>
2017-12-02 01:16:24 -0700

    dynamic radar chart

--- src/app/layout/social-vis/social-vis.component.ts
+++ src/app/layout/social-vis/social-vis.component.ts
@@ -12,7 +12,7 @@ import {Router} from '@angular/router';
 
 export class SocialVisComponent implements OnInit {
     user = JSON.parse(localStorage.getItem('currentUser')) || {};
-    userId = this.userId;
+    userId = this.user.id;
     // Opponent
     opponentId: string | any;
     radarChartData: any = [];
@@ -22,7 +22,7 @@ export class SocialVisComponent implements OnInit {
     opponentName;
     // Radar
     public radarChartLabels: string[] = ['Arrays', 'Methods', 'Operators', 'Strings', 'Variables'];
-    public radarChartType: string = 'radar';
+    public radarChartType = 'radar';
     public lineChartLabels: Array<any> = ['Quiz 1', 'Quiz 2', 'Quiz 3', 'Quiz 4', 'Quiz 5', 'Quiz 6', 'Quiz 7'];
     public lineChartOptions: any = {
         responsive: true
@@ -53,51 +53,145 @@ export class SocialVisComponent implements OnInit {
             pointHoverBorderColor: 'rgba(148,159,177,0.8)'
         }
     ];
-    public lineChartLegend: boolean = true;
-    public lineChartType: string = 'line';
-
+    public lineChartLegend = true;
+    public lineChartType = 'line';
 
     constructor(public router: Router, private backend: BackendService) {
+        this.getOpponent();
+        this.knowledgeGraph();
     }
 
     ngOnInit() {
-        this.getOpponent();
     }
 
-    proficiency() {
-    }
-
+    dataFlag1 = false;
     // events
     public chartClicked(e: any): void {
         // console.log(e);
     }
 
     getOpponent() {
-        let opponentName="";
+        let opponentName = '';
+        this.radarChartData= new Array();
         this.opponentId = localStorage.getItem('opponent');
         this.backend.getUser(this.opponentId).subscribe(
             opp => {
                 localStorage.setItem('opponentName', opp.fname);
-                console.log(opp);
                 this.opponentName = opp.fname;
                 opponentName = this.opponentName;
                 this.lineChartData = [
                     {data: [65, 59, 80, 81], label: 'You'},
                     {data: [28, 48, 40, 19, 86, 27, 90], label: localStorage.getItem('opponentName')},
                 ];
-                this.radarChartData = [
+          /*      this.radarChartData = [
                     {data: [75, 55, 80, 70, 86], label: 'You'},
                     {data: [60, 66, 77, 80, 76], label: localStorage.getItem('opponentName')}
-                ];
+                ];*/
                 this.dataFlag = true;
-                console.log(opponentName);
+
             });
+    }
+    elem1:any;
+    knowledgeGraph() {
+
+        this.backend.getKnowledgeValues(this.userId).subscribe(
+            (response) => {
+                let numberOfQuizzes = 0;
+                const radarChartMyData = new Array();
+                console.log(response);
+                let map = new Map();
+                for (let i = 0; i < response.length; i++) {
+                    if (!map.has(response[i].quizId)) {
+                        map.set(response[i].quizId, 1);
+                        numberOfQuizzes++;
+                    }
+                }
+                map=new Map();
+                for (let i = 0; i < response.length; i++) {
+                    if (!map.has(response[i].skillTopic)) {
+                        map.set(response[i].skillTopic, response[i].proficiency);
+                    }
+                    else {
+                        map.set(response[i].skillTopic, map.get(response[i].skillTopic) + response[i].proficiency);
+                    }
+                }
+                for (const entry of Array.from(map)) {
+                    if (entry[0] == 'Arrays')
+                        radarChartMyData[0] = (entry[1] / numberOfQuizzes);
+                    if (entry[0] == 'Methods')
+                        radarChartMyData[1] = (entry[1] / numberOfQuizzes);
+                    if (entry[0] == 'Operators')
+                        radarChartMyData[2] = (entry[1] / numberOfQuizzes);
+                    if (entry[0] == 'Strings')
+                        radarChartMyData[3] = (entry[1] / numberOfQuizzes);
+                    if (entry[0] == 'Variables')
+                        radarChartMyData[4] = (entry[1] / numberOfQuizzes);
+                }
+                 this.elem1 = {
+                    'data': radarChartMyData,
+                    'label': 'You'
+                };
+               // this.radarChartData.push(elem);this.dataFlag1=true;
+                //console.log(this.radarChartData);
+                this.knowledgeGraph1();
+            }
+        );
 
     }
 
-    public chartHovered(e: any): void {
+    knowledgeGraph1(){
+        this.backend.getKnowledgeValues(localStorage.getItem("opponent")).subscribe(
+
+            (response) => {
+                const radarChartMyData1 = new Array();
+                let numberOfQuizzes = 0;
+                console.log(response);
+                let map = new Map();
+                for (let i = 0; i < response.length; i++) {
+                    if (!map.has(response[i].quizId)) {
+                        map.set(response[i].quizId, 1);
+                        numberOfQuizzes++;
+                    }
+                }
+                console.log(map);
+                map = new Map();
+                for (let i = 0; i < response.length; i++) {
+                    if (!map.has(response[i].skillTopic)) {
+                        map.set(response[i].skillTopic, response[i].proficiency);
+                    }
+                    else {
+                        map.set(response[i].skillTopic, map.get(response[i].skillTopic) + response[i].proficiency);
+                    }
+                }
+                console.log(map);
+                for (const entry of Array.from(map)) {
+                    if (entry[0] == 'Arrays')
+                        radarChartMyData1[0] = (entry[1] / numberOfQuizzes);
+                    if (entry[0] == 'Methods')
+                        radarChartMyData1[1] = (entry[1] / numberOfQuizzes);
+                    if (entry[0] == 'Operators')
+                        radarChartMyData1[2] = (entry[1] / numberOfQuizzes);
+                    if (entry[0] == 'Strings')
+                        radarChartMyData1[3] = (entry[1] / numberOfQuizzes);
+                    if (entry[0] == 'Variables')
+                        radarChartMyData1[4] = (entry[1] / numberOfQuizzes);
+                }
+                const elem = {
+                    'data': radarChartMyData1,
+                    'label': localStorage.getItem('opponentName')
+                };
+                // this.radarChartData.push(elem1);
+                // this.radarChartData.push(elem);
+
+                this.radarChartData = [this.elem1,elem];
+                this.dataFlag1=true;
+                console.log(this.radarChartData);
+            }
+        );
+    }
+
+   public chartHovered(e: any): void {
         // console.log(e);
     }
-
 
 }
