b7ad94d6965dba9442a6bf81eaea6a1896b480e1
palakanmol31 <palak.anmol@gmail.com>
2017-12-03 03:27:56 -0700

    Changing scroll for leaderboard, adding headings for each visualization

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -178,7 +178,7 @@ export class DashboardComponent implements OnInit {
                     const name = response[i].category;
                     const series = new Array();
 
-                    const value1 = {'name': 'attempted', 'value': response[i].attempted};
+                    const value1 = {'name': 'attempted', 'value': response[i].attempted - response[i].correct};
                     const value2 = {'name': 'correct', 'value': response[i].correct};
 
                     series.push(value2);
